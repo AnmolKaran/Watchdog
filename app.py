b94c03c27485c51abf0f35769262a5c93b669878
Anmol Karan <anmolkaranva@gmail.com>
2024-02-25 07:36:58 -0500

    final everything

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, Response, jsonify
+#from webcamtest import pose_estimate,pose_estimate_secure
+#import mediapipe as mp
+#import cv2
+
+app = Flask(__name__)
+#camera = cv2.VideoCapture(1)
+
+app.debug = True
+
+
+@app.route('/')
+def login_page():
+    print('someone on page')
+    return render_template('login.html')
+
+
+@app.route('/loggedin')
+def loggedin():
+    return render_template('home.html')
+
+@app.route('/stream1')
+def stream():
+    print('stream')
+    return render_template('stream.html')
+# @app.route('/video-security-on')
+# def video():
+#     secure = True
+#     # print('reached')
+#     mp_pose = mp.solutions.pose
+#     pose = mp_pose.Pose()
+#     # print('video')
+#     return Response(generate_frames_secure(mp_pose,pose,secure),mimetype='multipart/x-mixed-replace;boundary=frame')
+
+@app.route('/video')
+def video():
+    # print('reached')
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
+    # print('video')
+    return Response(generate_frames(mp_pose,pose),mimetype='multipart/x-mixed-replace;boundary=frame')
+
+
+def generate_frames(mp_pose,pose):
+    while True:
+
+        returning,frame = camera.read()
+        
+        if not returning:
+            print('no stream')
+            break
+        else:
+            frame = pose_estimate(mp_pose=mp_pose,pose=pose,frame=frame)
+            ret,buffer = cv2.imencode('.jpg',frame)
+
+           
+            
+            frame = buffer.tobytes()
+
+        # return frame
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+# def generate_frames_secure(mp_pose,pose, secure):
+#     while True:
+
+#         returning,frame = camera.read()
+        
+#         if not returning:
+#             print('no stream')
+#             break
+#         else:
+#             frame = pose_estimate_secure(mp_pose=mp_pose,pose=pose,frame=frame,secure=secure)
+#             if str(type(frame))=='<class \'str\'>':
+                
+#             ret,buffer = cv2.imencode('.jpg',frame)
+
+           
+            
+#             frame = buffer.tobytes()
+
+#         # return frame
+#         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+if __name__ == '__main__':
+
+    app.run(host = '0.0.0.0', port = 80,debug=True)
+
+    
--- webcamtest.py
+++ webcamtest.py
@@ -2,23 +2,9 @@ import cv2
 import mediapipe as mp
 import math
 
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
 
-stream = cv2.VideoCapture(1)
-
-if not stream.isOpened():
-    print("no stream")
-    exit()
-
-count = 0
-while(True):
-    ret, frame = stream.read()
-    #cv2.imwrite("frame%d.jpg" % count, frame)
-    if not ret:
-        print("no more stream")
-        break
-
+def pose_estimate(mp_pose,pose,frame):
+    
     results = pose.process(frame)
     landmarks = results.pose_landmarks
 
@@ -28,26 +14,85 @@ while(True):
         right_hip = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
         left_shoulder = landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
         right_shoulder = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        left_wrist = landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST]
+        right_wrist = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
 
         hip_to_shoulder_vector = [right_shoulder.x - left_hip.x, right_shoulder.y - left_hip.y, right_shoulder.z - left_hip.z]
+        right_shoulder_to_wrist_vector = [right_shoulder.x - right_wrist.x, right_shoulder.y - right_wrist.y, right_shoulder.z - left_wrist.z]
+        left_shoulder_to_wrist_vector = [left_shoulder.x - left_wrist.x, left_shoulder.y-left_wrist.y, left_shoulder.z - left_wrist.z]
 
-        angle_with_upward = math.degrees(math.acos(hip_to_shoulder_vector[1] / math.sqrt(sum(x**2 for x in hip_to_shoulder_vector))))
-        cv2.putText(frame, f"Angle: {angle_with_upward:.2f} degrees", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        hs_angle_with_upward = math.degrees(math.acos(hip_to_shoulder_vector[1] / math.sqrt(sum(x**2 for x in hip_to_shoulder_vector))))
+        right_shoulder_to_wrist_angle = math.degrees(math.acos(right_shoulder_to_wrist_vector[1] / math.sqrt(sum(x**2 for x in right_shoulder_to_wrist_vector))))
+        # cv2.putText(frame, f"Angle: {hs_angle_with_upward:.2f} degrees", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(frame, f"Angle: {right_shoulder_to_wrist_angle:.2f} degrees", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        if angle_with_upward < 70:
+
+        if right_shoulder_to_wrist_angle < 100:
             cv2.putText(frame, "Collapsed", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
         for landmark in landmarks.landmark:
             height, width, _ = frame.shape
             cx, cy = int(landmark.x * width), int(landmark.y * height)
             cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
-    
-    cv2.imshow("Webcam", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-    count += 1
+    return frame
 
 
-stream.release()
-cv2.destroyAllWindows()
-    
+def pose_estimate_secure(mp_pose,pose,frame,secure):
+    if secure:
+
+        results = pose.process(frame)
+        landmarks = results.pose_landmarks
+
+        if landmarks:
+            return 'True'
+    else:
+        if landmarks:
+   # Extract key landmarks for the hips and shoulders
+            left_hip = landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
+            right_hip = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+            left_shoulder = landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
+            right_shoulder = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+
+            hip_to_shoulder_vector = [right_shoulder.x - left_hip.x, right_shoulder.y - left_hip.y, right_shoulder.z - left_hip.z]
+
+            angle_with_upward = math.degrees(math.acos(hip_to_shoulder_vector[1] / math.sqrt(sum(x**2 for x in hip_to_shoulder_vector))))
+            cv2.putText(frame, f"Angle: {angle_with_upward:.2f} degrees", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            if angle_with_upward < 70:
+                cv2.putText(frame, "Collapsed", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            for landmark in landmarks.landmark:
+                height, width, _ = frame.shape
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+                cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+        return frame
+
+
+if __name__ == '__main__':
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
+
+    stream = cv2.VideoCapture(1)
+
+    if not stream.isOpened():
+        print("no stream")
+        exit()
+        count = 0
+    while(True):
+        ret, frame = stream.read()
+        #cv2.imwrite("frame%d.jpg" % count, frame)
+        if not ret:
+            print("no more stream")
+            break
+
+        frame = pose_estimate(frame)
+        
+        cv2.imshow("Webcam", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        count += 1
+
+
+    stream.release()
+    cv2.destroyAllWindows()
+        
